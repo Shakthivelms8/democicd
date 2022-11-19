0e87fb73dec34acc76c2367ca0b777b5685eb780
kelzms8 <68555219+kelzms8@users.noreply.github.com>
2022-11-19 11:15:08 +0530

    Add files via upload

---
+++ democicd (1)/democicd/src/main/java/cicd_example/aws/DemocicdApplication.java
@@ -0,0 +1,13 @@
+package cicd_example.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocicdApplication.class, args);
+	}
+
+}
---
+++ democicd (1)/democicd/src/test/java/cicd_example/aws/DemocicdApplicationTests.java
@@ -0,0 +1,13 @@
+package cicd_example.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
